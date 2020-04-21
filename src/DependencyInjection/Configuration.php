7c2e3a63757ca71a92ddc408bf560620817624fe
Joachim Løvgaard <joachim@loevgaard.dk>
2020-04-21 15:24:52 +0200

    Add config example

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -4,23 +4,30 @@ declare(strict_types=1);
 
 namespace Acme\SyliusExamplePlugin\DependencyInjection;
 
+use function method_exists;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 final class Configuration implements ConfigurationInterface
 {
-    /**
-     * {@inheritdoc}
-     */
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder('acme_sylius_example_plugin');
-        if (\method_exists($treeBuilder, 'getRootNode')) {
+        $treeBuilder = new TreeBuilder('acme_sylius_example');
+        if (method_exists($treeBuilder, 'getRootNode')) {
             $rootNode = $treeBuilder->getRootNode();
         } else {
             // BC layer for symfony/config 4.1 and older
-            $rootNode = $treeBuilder->root('acme_sylius_example_plugin');
+            $rootNode = $treeBuilder->root('acme_sylius_example');
         }
+
+        $rootNode
+            ->children()
+                ->scalarNode('option')
+                    ->info('This is an example configuration option')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                ->end()
+        ;
 
         return $treeBuilder;
     }
