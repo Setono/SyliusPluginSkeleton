e462ca2605b83860d87d4a56dc1239a4c9aea749
Joachim Løvgaard <joachim@loevgaard.dk>
2026-04-20 11:56:12 +0200

    Move PHPStan ignore from config file to inline annotation

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -9,6 +9,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 final class Configuration implements ConfigurationInterface
 {
+    /** @phpstan-ignore missingType.generics (TreeBuilder is generic in Symfony >=7.1 but not in 6.4) */
     public function getConfigTreeBuilder(): TreeBuilder
     {
         return new TreeBuilder('acme_sylius_example');
