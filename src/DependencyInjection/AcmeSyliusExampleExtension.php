f783aa538bb94696dfdcde613628a4b37a5e2759
Joachim Løvgaard <joachim@loevgaard.dk>
2026-05-11 08:52:53 +0200

    Migrate service configuration from XML to PHP
    Switches the DI extension loader from XmlFileLoader to PhpFileLoader so
    service definitions use the typed PHP DSL going forward.

--- src/DependencyInjection/AcmeSyliusExampleExtension.php
+++ src/DependencyInjection/AcmeSyliusExampleExtension.php
@@ -7,15 +7,15 @@ namespace Acme\SyliusExamplePlugin\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 
 final class AcmeSyliusExampleExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
         $config = $this->processConfiguration($this->getConfiguration([], $container), $configs);
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $loader = new PhpFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
 
-        $loader->load('services.xml');
+        $loader->load('services.php');
     }
 }
